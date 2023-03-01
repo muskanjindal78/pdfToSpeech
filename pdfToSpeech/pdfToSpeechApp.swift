c7426a73ee96a18e05a168e1ce96880a60e958e6
Muskan Jindal <muskan.jindal@dhani.com>
2023-03-01 20:18:36 +0530

    Initial Commit

---
+++ pdfToSpeech/pdfToSpeechApp.swift
@@ -0,0 +1,17 @@
+//
+//  pdfToSpeechApp.swift
+//  pdfToSpeech
+//
+//  Created by Muskan Jindal on 01/03/23.
+//
+
+import SwiftUI
+
+@main
+struct pdfToSpeechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
